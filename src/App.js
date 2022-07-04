1fe7d03316801bac7e9a168bcdf727a8aa33943c
manan-dev <mananpatel6902@gmail.com>
2022-07-04 18:01:14 -0400

    added styling to name, title, email and buttons

--- src/App.js
+++ src/App.js
@@ -6,19 +6,32 @@ import {
 	faLinkedin,
 	faDiscord,
 } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
 	return (
 		<div className="container">
-				<img src={profile} alt="my selfie" className="profile-pic"></img>
+			<img src={profile} alt="my selfie" className="profile-pic"></img>
 			<div className="body--container">
 				<h2 className="name">Manan Patel</h2>
 				<h4 className="title">Frontend Developer</h4>
 				<h5 className="email">Mananpatel6902@gmail.com</h5>
 			</div>
 			<div className="social--buttons">
-				<button>Email</button>
-				<button>LinkedIn</button>
+				<button className="email--button">
+					<FontAwesomeIcon
+						className="email--icon"
+						icon={faEnvelope}
+					/>
+					Email
+				</button>
+				<button className="linkedin--button">
+					<FontAwesomeIcon
+						className="linkedin--icon"
+						icon={faLinkedin}
+					/>
+					LinkedIn
+				</button>
 			</div>
 			<div className="about--container">
 				<h4 className="about--title">About</h4>
